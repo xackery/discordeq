8f627728bfd8503ef7f31af8adcc2e1c02a16cb2
Xackery Xtal <xackery@gmail.com>
2016-01-28 11:34:10 -0800

    Revised menu options to add configuring Discord

--- main.go
+++ main.go
@@ -8,9 +8,10 @@ import (
 )
 
 func main() {
+	isFirstRun := true
 	for {
 		err := showMenu()
-		if err == nil { //Keep looping menu until no errors found
+		if isFirstRun && err == nil { //Keep looping menu until no errors found
 			break
 		}
 	}
@@ -24,7 +25,6 @@ func showMenu() (err error) {
 	config, err := eqemuconfig.LoadConfig()
 	status := "Good"
 	if err != nil {
-		isConfigLoaded = false
 		status = fmt.Sprintf("Bad (%s)", err.Error())
 	} else {
 		isConfigLoaded = true
@@ -32,11 +32,30 @@ func showMenu() (err error) {
 	}
 	fmt.Printf("1) Reload eqemu_config.xml (Status: %s)\n", status)
 	if isConfigLoaded {
+		isEverythingGood := true
+
 		status = "Good"
 		if config.Discord.Username == "" || config.Discord.Password == "" {
-			status = "Bad"
+			status = "Not configured"
+			isEverythingGood = false
 		}
-		fmt.Printf("2) Discord settings inside eqemu_config.xml (Status: %s)\n", status)
+		if config.Discord.ServerID == 0 && config.Discord.ChannelID == 0 {
+			status = "Bad"
+			isEverythingGood = false
+		}
+		fmt.Printf("2) Configure Discord settings inside eqemu_config.xml (Status: %s)\n", status)
+
+		status = "Good"
+		err := checkChatLog()
+		if err != nil {
+			isEverythingGood = false
+			status = "Bad: " + err.Error()
+		}
+		fmt.Printf("3) Enable Chat Logging (Status: %s)\n", status)
+		fmt.Printf("4) Quest File For Discord Chat (Status: %s)\n", status)
+		if isEverythingGood {
+			fmt.Println("5) Start DiscordEQ")
+		}
 	}
 	fmt.Println("Q) Quit")
 
@@ -50,5 +69,9 @@ func showMenu() (err error) {
 		fmt.Println("Invalid option")
 		err = fmt.Errorf("No option chosen")
 	}
+	return
+}
+
+func checkChatLog() (err error) {
 	return
 }
