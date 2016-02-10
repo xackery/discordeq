b88b6a05f79b4cf7f686a7a7e683d491b9d275aa
Xackery Xtal <xackery@gmail.com>
2016-02-10 11:15:27 -0800

    changed builder to go to discordeq

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 echo Building Linux
 GOOS=linux GOARCH=amd64 go build main.go
-mv main bin/eqcleanup-linux-x64
+mv main bin/discordeq-linux-x64
 GOOS=linux GOARCH=386 go build main.go
-mv main bin/eqcleanup-linux-x86
+mv main bin/discordeq-linux-x86
 echo Building Windows
 GOOS=windows GOARCH=amd64 go build main.go
-mv main.exe bin/eqcleanup-windows-x64.exe
+mv main.exe bin/discordeq-windows-x64.exe
 GOOS=windows GOARCH=386 go build main.go
-mv main.exe bin/eqcleanup-windows-x386.exe
+mv main.exe bin/discordeq-windows-x86.exe
 echo Building OSX
 GOOS=darwin GOARCH=amd64 go build main.go
-mv main bin/eqcleanup-darwin-x64
+mv main bin/discordeq-darwin-x64
