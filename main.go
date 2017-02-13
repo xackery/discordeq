24ad6a9cdc36d29b11e1e5237afbf04a09a86f29
Xackery <xackery@gmail.com>
2017-02-13 14:20:46 -0800

    added 0.42 tag.

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 }
 
 func startService() {
-	log.Println("Starting DiscordEQ v0.41")
+	log.Println("Starting DiscordEQ v0.42")
 	var option string
 	//Load config
 	config, err := eqemuconfig.GetConfig()
